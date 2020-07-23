10ee7ebb711c42fd6aa44a22e75d1ff4af7dbfda
heshubo <375187588@qq.com>
2020-07-23 11:56:07 +0800

    commit ace

---
+++ Client/Client.cpp
@@ -0,0 +1,99 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#include "ace/Message_Queue.h"
+#include "ace/Asynch_IO.h"
+#include "ace/OS.h"
+#include "ace/Proactor.h"
+#include "ace/Asynch_Connector.h"
+
+class Service_Handler : public ACE_Service_Handler
+{
+public:
+	Service_Handler()
+	{
+		ACE_OS::printf("Service_Handler constructed for connector \n");
+	}
+	~Service_Handler()
+	{
+		if (this->handle() != ACE_INVALID_HANDLE)
+			ACE_OS::closesocket(this->handle());
+		ACE_OS::printf("one Service_Handler for connecter destructed");
+	}
+
+	//把当前时间发送到远端
+	void send_to_accepter(void);
+
+	virtual void open(ACE_HANDLE h, ACE_Message_Block&)
+	{
+		//在OPEN函数中完成读写操作
+		this->handle(h);
+		//connect
+		if (this->writer_.open(*this) != 0)
+		{
+			ACE_ERROR((LM_ERROR, ACE_TEXT("%p\n"),
+				ACE_TEXT("Service_Handler open")));
+			delete this;
+			return;
+		}
+		ACE_OS::printf("connceted\n");
+		send_to_accepter();
+		return;
+	}
+
+	//writer_.write()异步写完成后会调用此函数
+	virtual void handle_write_stream
+	(const ACE_Asynch_Write_Stream::Result &result)
+	{
+		result.message_block().release();
+		//ACE_OS::sleep(1);//发送完成之后一定要暂停，给对方一个处理的时间。要不然一直发送对方接收不过来就会丢失。更为常见的情况不是暂停时间，而是接收对端发来的确认信息。就像本文一开始说的那样。
+		//上次发送完毕之后再接着发送一次,这次发送完成之后又会调用
+		//handle_write_stream，所以会一直发送
+		send_to_accepter();
+		return;
+	}
+
+private:
+	//用来向远端发送内容
+	ACE_Asynch_Write_Stream writer_;
+};
+//把当前时间发送到远端
+void Service_Handler::send_to_accepter(void)
+{
+	time_t now = ACE_OS::gettimeofday().sec();
+	ACE_Message_Block *mb = new ACE_Message_Block(4096);
+	//获取当前时间的字符串格式
+	std::string str = ctime(&now);
+	str += ":1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++!1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++1234567890abcdefghijklmnonqrstuvwxyzxyznowyousee,i can sing my abc++++end!";
+	mb->copy(str.c_str());
+	//send message to accepter
+	if (this->writer_.write(*mb, mb->length()) != 0)
+	{
+		ACE_OS::printf("Begin write fail in open\n");
+		delete this;
+		return;
+	}
+	else
+	{
+		ACE_OS::printf("sended %s\n", mb->rd_ptr());
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	ACE_INET_Addr addr;
+	addr.set(3000, ACE_LOCALHOST);
+
+	char addr_buffer[50] = { 0 };
+	addr.addr_to_string(addr_buffer, sizeof(addr_buffer));
+	cout << "connecting[" << addr_buffer << "]...." << endl;
+
+	ACE_Asynch_Connector<Service_Handler> connector;
+	connector.open();
+	if (connector.connect(addr) == -1)
+		return -1;
+
+	ACE_Proactor::instance()->proactor_run_event_loop();
+
+	return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,87 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+using namespace std;
+#include "ace/Message_Queue.h"
+#include "ace/Asynch_IO.h"
+#include "ace/OS.h"
+#include "ace/Proactor.h"
+#include "ace/Asynch_Acceptor.h"
+
+class Service_Handler : public ACE_Service_Handler
+{
+public:
+	Service_Handler()
+	{
+		ACE_OS::printf("Service_Handler constructed for accepter \n");
+	}
+	~Service_Handler()
+	{
+		if (this->handle() != ACE_INVALID_HANDLE)
+			ACE_OS::closesocket(this->handle());
+		ACE_OS::printf("one Service_Handler for accepter destructed");
+	}
+	//异步读取消息
+	void reveive_from_connector(void);
+
+	virtual void open(ACE_HANDLE h, ACE_Message_Block&)
+	{
+		this->handle(h);
+		if (this->reader_.open(*this) != 0)
+		{
+			ACE_ERROR((LM_ERROR, ACE_TEXT("%p\n"),
+				ACE_TEXT("Service_Handler open")));
+			delete this;
+			return;
+		}
+		//异步读取
+		reveive_from_connector();
+		return;
+	}
+
+	//reader_.read()异步读完成后会调用此函数
+	virtual void handle_read_stream
+	(const ACE_Asynch_Read_Stream::Result &result)
+	{
+		ACE_Message_Block &mb = result.message_block();
+		if (!result.success() || result.bytes_transferred() == 0)
+		{
+			mb.release();
+			delete this;
+			return;
+		}
+		//输出读取内容
+		ACE_OS::printf("received:%s\n", mb.rd_ptr());
+		mb.release();
+		//继续下一次异步读取
+		reveive_from_connector();
+	}
+
+private:
+	ACE_Asynch_Read_Stream reader_;
+	char buffer[8192];
+};
+
+void Service_Handler::reveive_from_connector(void)
+{
+	ACE_Message_Block *mb = new ACE_Message_Block(buffer, 4096);
+	if (this->reader_.read(*mb, mb->space()) != 0)
+	{
+		ACE_OS::printf("Begin read fail\n");
+		delete this;
+		return;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int port = 3000;
+	ACE_Asynch_Acceptor<Service_Handler> acceptor;
+
+	if (acceptor.open(ACE_INET_Addr(port)) == -1)
+		return -1;
+	cout << "listenning localhost[" << port << "]...." << endl;
+
+	ACE_Proactor::instance()->proactor_run_event_loop();
+
+	return 0;
+}
